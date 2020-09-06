57d0d4be8c32444717eec6340875ef7ab6578114
borabora0809 <borabora0809@gmail.com>
2020-09-06 21:20:30 +0900

    AjaxAppの準備

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,4 @@
 Rails.application.routes.draw do
-  devise_for :users
-  root to: 'tweets#index'
-  resources :tweets do
-    resources :comments, only: :create
-    collection do
-      get 'search'
-    end
-  end
-  resources :users, only: :show
+  root to: 'posts#index'
+  post 'posts', to: 'posts#create'
 end
